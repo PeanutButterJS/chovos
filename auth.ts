36d9fe07fcbc6f4a4f1271f29a57f9c4b848afe4
Aron Weinraub <pntbtrjs@gmail.com>
2024-10-07 08:03:42 +0300

    try 3

--- auth.ts
+++ auth.ts
@@ -4,8 +4,8 @@ import Google from 'next-auth/providers/google';
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google({
-      clientId: '276321684558-9mse6d8gjr2gkdq4sff5g99sjn038pv8.apps.googleusercontent.com',
-      clientSecret: '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'
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
   ],
   secret: process.env.NEXT_PUBLIC_SECRET // ran this online openssl rand -base64 32
